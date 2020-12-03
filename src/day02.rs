7648e82fefbe76bd372a06b811a78c8cc2d26578
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-12-02 21:19:07 -0800

    WIP day2 pt2

--- src/day02.rs
+++ src/day02.rs
@@ -1,12 +1,14 @@
-use std::io::Error;
+pub fn main() -> (u32, u32) {
+    (part_one(), 0)
+}
 
-pub fn main() -> Result<u32, Error> {
+fn part_one() -> u32 {
     let lines: Vec<String> = advent_of_code_2020::entries("day02.txt").unwrap();
     let mut total_count = 0;
     for line in lines {
         let line: Vec<&str> = line.split(' ').collect();
-        let rng: Vec<&str> = line[0].split('-').collect();
-        let rng: (i8, i8) = (rng[0].parse::<i8>().unwrap(), rng[1].parse::<i8>().unwrap());
+        let range: Vec<&str> = line[0].split('-').collect();
+        let range: (i8, i8) = (range[0].parse::<i8>().unwrap(), range[1].parse::<i8>().unwrap());
         let ltr: Vec<&str> = line[1].split(':').collect();
         let ltr: char = ltr[0].parse().unwrap();
         let password = line[2];
@@ -16,10 +18,10 @@ pub fn main() -> Result<u32, Error> {
                 count += 1;
             }
         }
-        let (lo, hi) = rng;
+        let (lo, hi) = range;
         if lo <= count && count <= hi {
             total_count += 1;
         }
     }
-    Ok(total_count)
+    total_count
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ mod day02;
 
 fn main() {
     println!("Day 01 answer: {:?}", day01::main().unwrap());
-    println!("Day 02 answer: {:?}", day02::main().unwrap());
+
+    let (day2_pt1, day2_pt2) = day02::main();
+    println!("Day 02/1 answer: {:?}", day2_pt1);
+    println!("Day 02/2 answer: {:?}", day2_pt2);
 }
