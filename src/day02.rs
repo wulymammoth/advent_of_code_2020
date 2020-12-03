7c0265963f8bad82f748ac9abacd8795ea0f7f4a
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-12-02 21:12:52 -0800

    Day 02 - Password Philosophy/1

--- src/day02.rs
+++ src/day02.rs
@@ -1,19 +1,25 @@
 use std::io::Error;
 
-pub fn main() -> Result<u8, Error> {
+pub fn main() -> Result<u32, Error> {
     let lines: Vec<String> = advent_of_code_2020::entries("day02.txt").unwrap();
-    //let entries: Vec<Tuple> = Vec::new();
-    for line in lines.iter() {
-        let [range, letter, password] = line.split(' ').collect();
-        println!("{:?}", );
+    let mut total_count = 0;
+    for line in lines {
+        let line: Vec<&str> = line.split(' ').collect();
+        let rng: Vec<&str> = line[0].split('-').collect();
+        let rng: (i8, i8) = (rng[0].parse::<i8>().unwrap(), rng[1].parse::<i8>().unwrap());
+        let ltr: Vec<&str> = line[1].split(':').collect();
+        let ltr: char = ltr[0].parse().unwrap();
+        let password = line[2];
+        let mut count = 0;
+        for ch in password.chars() {
+            if ch == ltr {
+                count += 1;
+            }
+        }
+        let (lo, hi) = rng;
+        if lo <= count && count <= hi {
+            total_count += 1;
+        }
     }
-    Ok(1)
+    Ok(total_count)
 }
-
-//fn password_philosophy(entries: Vec<Tuple>) -> u8 {
-    //for entry in entries.iter() {
-        //let [range, letter, password] = entry;
-        //println!("range: {} | letter: {} | password: {}", range, letter, password);
-    //}
-    //0
-//}
