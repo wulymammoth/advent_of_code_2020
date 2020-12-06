87971083294a47487c2c596fb36644ef17956c03
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-12-06 08:45:35 -0800

    Drop prints in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-mod day01;
-mod day02;
-
 fn main() {
-    println!("Day 01 answer: {:?}", day01::main().unwrap());
-
-    let (day02_pt1, day02_pt2) = day02::main();
-    println!("Day 02/1 answer: {:?}", day02_pt1);
-    println!("Day 02/2 answer: {:?}", day02_pt2);
+    // Nothing to see here
 }
