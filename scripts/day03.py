c303184f08b5c6e637270a8e3ad283b33184614c
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-12-12 21:37:57 -0800

    complete day 3, part 1

--- scripts/day03.py
+++ scripts/day03.py
@@ -4,29 +4,25 @@ def solve():
         lines = [line.strip() for line in f.readlines()]
 
 def part1(lines):
-    num_lines         = len(lines)
+    total_lines       = len(lines)
     line_length       = len(lines[0])
     trees_encountered = 0
 
-    def traverse(i, j, remaining):
-        nonlocal trees_encountered
+    i, j, remaining = 0, 0, 4
+    while i < total_lines:
+        if j == line_length: # reset to beginning of the line (row)
+            j = 0
 
-        # terminal condition
-        if i == num_lines:
-            return
-
-        if remaining == 0: # remaining is zero
+        if remaining == 0:
             if lines[i][j] == '#':
                 trees_encountered += 1
-            j = 0 if j == line_length - 1 else j + 1
-            traverse(i, j, 3)
-        elif remaining == 1: # remaining > 0
-            traverse(i + 1, j, remaining - 1)
-        else: # remaining is 2 or greater
-            j = 0 if j == line_length - 1 else j + 1
-            traverse(i, j + 1, remaining - 1)
+            remaining = 4 # reset
 
-    traverse(0, 0, 0)
+        if remaining == 1: # go down
+            i += 1
+        else: # remaining is zero or greater than one
+            j += 1
+        remaining -= 1
 
     return trees_encountered
 
@@ -36,18 +32,27 @@ if __name__ == '__main__':
 import unittest
 
 class TestTobogganTrajectory(unittest.TestCase):
-    def test_simple(self):
-        fp = "day03.txt"
-        with open(fp, 'r') as f:
-            lines = [line.strip() for line in f.readlines()]
-
-        lines = lines[:3]
-        for line in lines: print(line)
-        self.assertEqual(part1(lines), 2)
+    def test_basic(self):
+        lines = [
+            list(line) for line in [
+                '..##.......',
+                '#...#...#..',
+                '.#....#..#.',
+                '..#.#...#.#',
+                '.#...##..#.',
+                '..#.##.....',
+                '.#.#.#....#',
+                '.#........#',
+                '#.##...#...',
+                '#...##....#',
+                '.#..#...#.#'
+            ]
+        ]
+        self.assertEqual(part1(lines), 7)
 
     def test_full(self):
         fp = "day03.txt"
         with open(fp, 'r') as f:
             lines = [line.strip() for line in f.readlines()]
 
-        self.assertEqual(part1(lines), 2)
+        self.assertEqual(part1(lines), 294)
