c00cf4707c9ad69eaeab65874860ad1ebe6947cb
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-12-14 05:05:00 -0800

    complete day04, part 2

--- python/day04.py
+++ python/day04.py
@@ -62,9 +62,11 @@ VALIDATORS = {
     'pid': valid_passport_id
 }
 
-def valid(document):
+def valid(document, check_fields=False):
     for req_field in REQUIRED_FIELDS:
         if req_field not in document and req_field != 'cid':
+            return False
+        if check_fields and req_field != 'cid' and not VALIDATORS[req_field](document[req_field]):
             return False
     return True
 
@@ -92,7 +94,7 @@ def part1(batch):
     return len(list(filter(valid, passports(batch))))
 
 def part2(batch):
-    return len(list(filter(valid, passports(batch))))
+    return len(list(filter(lambda doc: valid(doc, True), passports(batch))))
 
 import unittest
 
@@ -121,7 +123,7 @@ class TestPassportProcessing(unittest.TestCase):
     def test_part1_full(self):
         with open('day04.txt', 'r') as f:
             batch = [line.strip() for line in f.readlines()]
-        self.assertEqual(part1(batch), 204) # too low
+        self.assertEqual(part1(batch), 204)
 
     def test_byr_validator(self):
         self.assertTrue(VALIDATORS['byr']('2002'))
@@ -142,6 +144,25 @@ class TestPassportProcessing(unittest.TestCase):
         self.assertTrue(VALIDATORS['pid']('000000001'))
         self.assertFalse(VALIDATORS['pid']('0123456789'))
 
-    @unittest.skip
+    def test_part2_basic(self):
+        batch = [
+            'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980',
+            'hcl:#623a2f',
+            '',
+            'eyr:2029 ecl:blu cid:129 byr:1989',
+            'iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm',
+            '',
+            'hcl:#888785',
+            'hgt:164cm byr:2001 iyr:2015 cid:88',
+            'pid:545766238 ecl:hzl',
+            'eyr:2022',
+            '',
+            'iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719',
+        ]
+        valid_passports = 4
+        self.assertEqual(part2(batch), valid_passports)
+
     def test_part2_full(self):
-        pass
+        with open('day04.txt', 'r') as f:
+            batch = [line.strip() for line in f.readlines()]
+        self.assertEqual(part2(batch), 179)
