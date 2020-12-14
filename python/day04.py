a1dca2f571c8dd41485b92c3aae7ac665522c3f4
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-12-13 18:05:26 -0800

    WIP day04, part 2

--- python/day04.py
+++ python/day04.py
@@ -9,6 +9,58 @@ REQUIRED_FIELDS = [
     'cid', # country ID
 ]
 
+EYE_COLORS = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+
+def valid_height(height):
+    digits = []
+    for i, char in enumerate(height):
+        if char.isdigit():
+            digits.append(char)
+        elif char.isalpha():
+            if i < 3: # we must have '#' and at least two digits
+                return False
+            suffix = height[i:]
+            if suffix not in ['cm', 'in']:
+                return False
+            h = int(''.join(digits))
+            return 150 <= h <= 193 if suffix == 'cm' else 50 <= h <= 76
+        else: # non-alphanumeric
+            return False
+    return False
+
+def valid_hair_color(hair):
+    'number followed by exactly six chars that are 0-9 or a-f' # assuming case sensitivity here
+    prefix, suffix = hair[:len(hair)-6], hair[-6:]
+    if prefix[0] != '#':
+        return False
+    for char in prefix:
+        if not char.isdigit():
+            return False
+    for char in suffix:
+        if not char.isalnum():
+            return False
+        if char.isalpha() and ord(char) not in range(ord('a'), ord('f') + 1):
+            return False
+    return True
+
+def valid_passport_id(pid):
+    if len(pid) < 9:
+        return False
+    for digit in pid:
+        if not digit.isdigit():
+            return False
+    return True
+
+VALIDATORS = {
+    'byr': lambda year: len(year) >= 4 and 1920 <= int(year) <= 2002,
+    'iyr': lambda year: len(year) >= 4 and 2010 <= int(year) <= 2020,
+    'eyr': lambda year: len(year) >= 4 and 2020 <= int(year) <= 2030,
+    'hgt': valid_height,
+    'hcl': valid_hair_color,
+    'ecl': lambda eye_color: eye_color in EYE_COLORS,
+    'pid': valid_passport_id
+}
+
 def valid(document):
     for req_field in REQUIRED_FIELDS:
         if req_field not in document and req_field != 'cid':
@@ -38,6 +90,9 @@ def passports(batch):
 def part1(batch):
     return len(list(filter(valid, passports(batch))))
 
+def part2(batch):
+    return len(list(filter(valid, passports(batch))))
+
 import unittest
 
 class TestPassportProcessing(unittest.TestCase):
@@ -58,11 +113,36 @@ class TestPassportProcessing(unittest.TestCase):
             'iyr:2011 ecl:brn hgt:59in' # invalid; missing `cid` and `byr` fields
         ]
 
-    def test_basic(self):
+    def test_part1_basic(self):
         valid_passports = 2
         self.assertEqual(part1(self.batch), valid_passports)
 
-    def test_full(self):
+    def test_part1_full(self):
         with open('day04.txt', 'r') as f:
             batch = [line.strip() for line in f.readlines()]
         self.assertEqual(part1(batch), 204) # too low
+
+    def test_byr_validator(self):
+        self.assertTrue(VALIDATORS['byr']('2002'))
+        self.assertFalse(VALIDATORS['byr']('2003'))
+
+    def test_hgt_validator(self):
+        self.assertTrue(VALIDATORS['hgt']('60in'))
+        self.assertTrue(VALIDATORS['hgt']('190cm'))
+        self.assertFalse(VALIDATORS['hgt']('190in'))
+        self.assertFalse(VALIDATORS['hgt']('190'))
+
+    @unittest.skip
+    def test_hcl_validator(self):
+        self.assertTrue(VALIDATORS['hcl']('#123abc'))
+        self.assertFalse(VALIDATORS['hcl']('123abc'))
+        self.assertFalse(VALIDATORS['hcl']('#123abz'))
+
+    @unittest.skip
+    def test_pid_validator(self):
+        self.assertTrue(VALIDATORS['pid']('000000001'))
+        self.assertFalse(VALIDATORS['pid']('0123456789'))
+
+    @unittest.skip
+    def test_part2_full(self):
+        pass
